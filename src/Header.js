8cc43eea9a27e23610423efb5e5df1ec859daf27
Pankaj Desai <desaipankaj740@gmail.com>
2022-07-29 21:44:46 +0530

    done

--- src/Header.js
+++ src/Header.js
@@ -8,11 +8,18 @@ import {
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
 
-  const[{ basket }] = useStateValue();
-  console.log(basket);
+  const[{ basket,user }] = useStateValue();
+
+  const login = () =>{
+    if (user){
+      auth.signOut();
+    }
+  }
+ // console.log(basket);
   return (
 
     <nav className='header'>
@@ -31,10 +38,10 @@ function Header() {
 
       {/* 3 links  */}
       <div className="header__nav">
-        <Link to="/login" className='header__link'>
-          <div className="header__option">
-            <span className='header__lineone'>Hello Pankaj</span>
-            <span className="header__linetwo">Sign in</span>
+        <Link to={!user && "/login"} className='header__link'>
+          <div onClick={login} className="header__option">
+            <span className='header__lineone'>Hello {user?.email}</span>
+            <span className="header__linetwo">{user ? 'Sign out':'Sign in'}</span>
           </div>
         </Link>
 
